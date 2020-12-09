8249c4c3edc07c27bfffbc1ae5fe638efa3c12e1
aadit0215 <75716982+aadit0215@users.noreply.github.com>
2020-12-09 14:57:26 +0700

    Update index.js

--- index.js
+++ index.js
@@ -13,9 +13,9 @@ const tambahan = require("./lib/tambahan.js")
 const donasi = require("./lib/donasi.js");
 const info = require("./lib/info.js");
 /////////////////
-const BotName = 'REUZ BOT☄'; 
-const instagram = 'https://instagram.com/reuz.id'; 
-const telegram = 'https://t.me/reuzzz'; 
+const BotName = 'FAJAR BOT'; 
+const instagram = 'https://instagram.com/aadit0215'; 
+const telegram = 'https://t.me/rkmiyo'; 
 const kapanbotaktif = 'KETIKA HANYA DIAKTIFKAN'; 
 const youtube = 'https://www.youtube.com/c/reuz_id';
 //const grupch1 = 'belum ada grup'; 
@@ -68,7 +68,7 @@ conn.on('user-presence-update', json => console.log(json.id + ' presence is => '
 conn.on('message-status-update', json =>
 {
    const participant = json.participant ? ' (' + json.participant + ')' : '' // participant exists when the message is from a group
-   console.log(`[ ${moment().format("HH:mm:ss")} ] => bot by ig:@reuz.id`)
+   console.log(`[ ${moment().format("HH:mm:ss")} ] => bot by ig:@aadit0215`)
 })
 
 conn.on('message-new', async(m) =>
